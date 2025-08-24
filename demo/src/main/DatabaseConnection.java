d361a4f45af0fd4bb0b843b7e55ab525f73b5470
Jimmy Roy <jimmy.roy2006@gmail.com>
2025-08-24 16:19:29 -0400

    updated db connection

---
+++ demo/src/main/DatabaseConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/url_shortener_db";
+    private static final String USER = "url_user";
+    private static final String PASSWORD = "password123";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
